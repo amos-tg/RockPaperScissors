c0c6adc5c1466b7b2adaefa155525dd067e55941
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-01-20 12:15:06 -0500

    Tentative fn sig's and light documentation
    Outlining how I want to write out the program with likely to change
    function signatures and documentation as well as two score tracking
    variables which are unlikely to change.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
+#include <random>
 #include <iostream>
 
-int main(void) {
-  std::cout << "Hello World!" << std::endl;
+/// four constants for each possible choice (0..=3)
+enum { ROCK, PAPER, SCISSORS, QUIT }; 
 
+/// generates and returns a random number x with the following guarentees: 
+/// (x >= 0 && x < 3), these numbers are mapped directly onto the possible
+/// enum variants; computer cannot return quit/3.
+int getRandRPS(void);
+
+/// returns the users choice as an integer with the following guarentees: 
+/// (x >= 0 && x <= 3), these numbers are mapped directly onto the four possible
+/// enum variants.
+int getUserRPS(void);
+
+void compareAndTally(
+    int user_choice, int computer_choice, int user_score, int comp_score);
+
+void quitAndDisplay(int user_score, int comp_score);
+
+int main(void) {
+  int user_score {};
+  int comp_score {};
+  
   return 0;
 }
