31a100eeb0e268d2b71daba0fe89fed551b4e312
Amos Thacker-Gwaltney <amostg3613@gmail.com>
2026-01-22 10:45:03 -0500

    fleshout main fn; fns getRandRanged getUserChoice
    main setup; getRandRanged function returns
    a platform based random number across a given range specified by
    arguments. getUserChoice pulls a string from cin validates the input and
    returns an int which maps to an enum of the available choices.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <random>
 #include <iostream>
+#include <string>
+#include <cctype>
 
 /// four constants for each possible choice (0..=3)
 enum { ROCK, PAPER, SCISSORS, QUIT }; 
 
 /// generates and returns a random number x with the following guarentees: 
-/// (x >= 0 && x < 3), these numbers are mapped directly onto the possible
-/// enum variants; computer cannot return quit/3.
-int getRandRPS(void);
+/// (x >= range_start && x <= range_end)
+int getRandRanged(int range_start, int range_end);
 
-/// returns the users choice as an integer with the following guarentees: 
-/// (x >= 0 && x <= 3), these numbers are mapped directly onto the four possible
-/// enum variants.
-int getUserRPS(void);
+int getUserChoice(void);
 
 void compareAndTally(
     int user_choice, int computer_choice, int user_score, int comp_score);
@@ -23,16 +21,60 @@ int main(void) {
   int user_score, comp_score, user_choice, comp_choice;
   
   for (;;) {
-    user_choice = getUserRPS();
+    user_choice = getUserChoice();
 
     if (user_choice == QUIT) {
       break;
     }
 
-    comp_choice = getRandRPS();
+    comp_choice = getRandRanged(ROCK, SCISSORS);
 
     compareAndTally(user_choice, comp_choice, user_score, comp_score);
   }
   
   return 0;
+}
+
+int getRandRanged(int range_start, int range_end) {
+  // The purpose of the program is to generate these random responses for the
+  // user, it will be doing this for the life of the program; it simplifies the
+  // function if the random_device just gets stored for the life of the program.
+  static std::random_device rand;
+  std::uniform_int_distribution<int> distrib(range_start, range_end);
+
+  return distrib(rand);
+}
+
+int getUserChoice(void) {
+  const char *query = 
+    "Rock Paper Scissors:\n- Rock\n- Paper\n- Scissors\n- Quit";
+  const char *err_msg =
+    "Error: invalid user input"; 
+
+  std::cout << query << std::endl;
+
+  std::string input; 
+
+  std::cin >> input; 
+  if (!std::cin) {
+    std::cerr << err_msg << std::endl;
+    exit(1);
+  } 
+
+  for (char &c: input) { 
+    c = std::tolower(c);
+  }
+
+  if (input == "rock") {
+    return ROCK;
+  } else if (input == "paper") {
+    return PAPER;
+  } else if (input == "scissors") {
+    return SCISSORS;
+  } else if (input == "quit") {
+    return QUIT;
+  } else {
+    std::cerr << err_msg << std::endl;
+    exit(1); 
+  }
 }
